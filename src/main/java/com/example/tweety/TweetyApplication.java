17b8d82c77958dbe083dbc0b5ddba16b2a2ae7d0
shariorfarhan07 <shariorfarhan77@gmail.com>
2025-02-16 15:51:59 +0600

    Project init

---
+++ src/main/java/com/example/tweety/TweetyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tweety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tweety/TweetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tweety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
